50a89d8087b834edfd5a4c3927d2050c23d7ed3a
Kostadin Mihaylov <kmihaylov@90gmail.com>
2023-03-11 23:09:25 +0200

    Add solution for the cheating printer problem

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-import "./styles.css";
 import { dataFile } from "./data.js";
 
 function solution() {
-  // write your code here
-  // expected output: string containing number of
-  // tickets and total loss
+  const data = dataFile.split("\n");
+  let tickets = [];
+  let seatNumber = "";
   let duplicateSalesCount = 0;
   let duplicateTicketPrice = 0;
+  let price = 0;
+  let seatSearchTerm = "SEAT:";
+  let priceSearchTerm = "PRICE";
+
+  data.forEach((line) => {
+    if (line.includes(priceSearchTerm)) {
+       price = parseFloat(line.replace(priceSearchTerm, "").trim());
+    }
+    if (line.includes(seatSearchTerm)) {
+      seatNumber = line.replace(seatSearchTerm, "").trim();
+      if (!tickets.includes(seatNumber)) {
+        tickets.push(seatNumber);
+      }
+      else{
+        duplicateSalesCount++;
+        duplicateTicketPrice += price;
+      }
+    }
+  });
 
   return (
     `number duplicate ticket sales: ${duplicateSalesCount}` +
-    ` total loss: ${duplicateTicketPrice}`
+    ` total loss: ${duplicateTicketPrice.toFixed(2)}`
   );
 }
 
